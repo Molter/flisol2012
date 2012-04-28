208a6e1098dccea917c5ea6cc5bc42c81fc45543
Gabriel <gabriel.molter@gmail.com>
2012-04-28 08:50:27 -0300

    adicionado funcoes no controller

--- application/controllers/jokempo.php
+++ application/controllers/jokempo.php
@@ -1,11 +1,20 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Jokempo extends CI_CONTROLLER {
-	
+	//@todo Listar
 	public function index(){
 		$this->load->view('index');
 	}
-
+	
+	//@todo Criar e Editar
+	public function jogada($id = null){
+		$this->load->view('jogada');
+	}
+	
+	////@todo Deletar
+	public function delete($id){
+//		$this->load->view('index');
+	}
 }
 
 ?>
