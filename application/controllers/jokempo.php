0e135bab7c9c40125fa1e947ce81c6b2048693da
Gabriel <gabriel.molter@gmail.com>
2012-04-28 08:48:10 -0300

    adicionado controller

---
+++ application/controllers/jokempo.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Jokempo extends CI_CONTROLLER {
+	
+	public function index(){
+		$this->load->view('index');
+	}
+
+}
+
+?>
